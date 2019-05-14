89775d8a60b9119b8a0d49cc774d49692f545301
ChengDong Gao <538252@ad.seagate.com>
2019-05-14 08:13:57 +0800

    今天继续

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 import markdown
+from comments.forms import CommentForm
 
 
 
@@ -23,6 +24,16 @@ def detail(request, pk):
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
+    # 记得在顶部导入 CommentForm
+    form = CommentForm()
+    # 获取这篇 post 下的全部评论
+    comment_list = post.comment_set.all()
+
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
     return render(request, 'blog/detail.html', context={'post': post})
 
 def archives(request, year, month):
