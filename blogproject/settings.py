e6899c95915a8498bd4ba42c70adc986d216446d
ChengDong Gao <538252@ad.seagate.com>
2019-05-17 10:22:52 +0800

    好几天没做了，网站备案真麻烦！

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=(j)viusr-2v4#7y81uz3jaq7+&vkpv+)^yh0z-y3$wu*gcy16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.shishutong.xyz']
 
 
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
